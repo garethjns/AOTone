39c2af1670ef54330a342b881e94469286eb9749
Gareth Jones <garethjns4@gmail.com>
2017-07-02 11:24:52 +0100

    Update audioplayer example

--- AOExamples.m
+++ AOExamples.m
@@ -20,14 +20,28 @@ plot(tVec, amp*env)
 
 
 %% Create audioplayer object
+% Creates audiploayer object to handle stimulus and hardware.
 % Created with tone passed to L and R channels, and sample rate used to
 % generate tone.
 
-ao = audioplayer([tone;tone], Fs);
+ID = -1; % Default device (probably speakers or headphones)
+nBits = 24; % Output bits
+
+ao = audioplayer([tone;tone], Fs, nBits, ID);
 disp(ao)
 
 
 %% Play tone
-% Calling the play method output the tone
+% Calling the play method to output the tone
+% This will not block later code from running.
 
 ao.play()
+disp('This text is displayed while the output is playing.')
+
+
+%% Play tone
+% Calling the playlBocking method to output the tone
+% This will block later code from running.
+
+ao.playblocking()
+disp('This text is not displayed until after the output has finished.')
